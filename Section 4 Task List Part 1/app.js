fc8e8366b04e0bb97504be49c2af857fb59fbbcd
Jona Hyun <jona.hyun@gmail.com>
2019-02-19 10:01:51 -0500

    chapter 4 lesson 1 WIP

--- Section 4 Task List Part 1/app.js
+++ Section 4 Task List Part 1/app.js
@@ -1,45 +1,46 @@
-// Define UI Vars
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
+// Define UI variables
+const form = document.querySelector('#task-form'); // Form
+const taskInput = document.querySelector('#task'); // New task input
+const filter = document.querySelector('#filter'); // Filter input field
+const taskList = document.querySelector('.collection'); // ul
+const clearBtn = document.querySelector('.clear-tasks'); //button
 
 // Load all event listeners
-loadEventListeners();
+loadEventListener();
 
-// Load all event listeners
-function loadEventListeners() {
+// Load 
+function loadEventListener() {
   // Add task event
   form.addEventListener('submit', addTask);
-}
 
+}
 // Add Task
 function addTask(e) {
+  // if empty value
   if(taskInput.value === '') {
-    alert('Add a task');
-  }
+    alert('add a task');
+  } 
 
   // Create li element
   const li = document.createElement('li');
-  // Add class
+  // Add Class
   li.className = 'collection-item';
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
+
   // Create new link element
   const link = document.createElement('a');
   // Add class
   link.className = 'delete-item secondary-content';
-  // Add icon html
   link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Append the link to li
   li.appendChild(link);
 
-  // Append li to ul
+  //Append li to ul
   taskList.appendChild(li);
 
   // Clear input
   taskInput.value = '';
+
 
   e.preventDefault();
 }
